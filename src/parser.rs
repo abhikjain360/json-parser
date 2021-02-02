5972dce475fb9a677d33950139d8a56c86796f82
Abhik Jain <abhikjain360@gmail.com>
2021-02-02 12:52:17 +0530

    tests

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,20 @@
 use crate::lexer::Lexer;
+use crate::tokens::Tokens;
+
+use std::collections::VecDeque;
 
 struct Parser<'lexer> {
     lexer: Lexer<'lexer>,
+    stack: VecDeque<Tokens>,
+}
+
+impl<'lexer> Parser<'lexer> {
+    pub fn new<'a>(src: &'a str) -> Parser<'a> {
+        Parser {
+            lexer: Lexer::new(src),
+            stack: VecDeque::new(),
+        }
+    }
 }
 
 // TODO: write some tests here.
